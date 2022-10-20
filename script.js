4ff5fc46ece21e83ebed2a4abf00c79b57ee868c
Albin Lundström <albin.wilhelm.erik@gmail.com>
2022-10-20 13:05:54 +0200

    Lunchcommit, nästan där Lade till- funktion som skapar frågor för att question objekt i true/false arrayen måste kanske tänka om gällade VG kriterierna

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-let theQuestions = [
+let monoQuestions = [
     {
-        question: "What is The thing?",
-        option1: "Something",
-        option2: "Else"
+        question: "The earth is flat",
+
+        options: {
+            opt1: "True",
+            opt2: "False"
+
+        },
+        answer: "False"
     },
     {
         question: "What is da thingg?",
 
         options: {
-            opt1: "idk",
-            opt2: "okey",
-            opt3: "rofl",
-            op4: "kek"
+            opt1: "True",
+            opt2: "False"
         },
         answer: "opt1"
     },
@@ -19,19 +22,100 @@ let theQuestions = [
         question: "Testing",
 
         options: {
-            opt1: "idk",
-            opt2: "okey",
-            opt3: "rofl",
-            op4: "kek"
+            opt1: "True",
+            opt2: "False"
         },
         answer: "opt1"
-    }
+    },
+    {
+        question: "The earth is flat",
+
+        options: {
+            opt1: "True",
+            opt2: "False"
+
+        },
+        answer: "False"
+    },
+    {
+        question: "What is da thingg?",
+
+        options: {
+            opt1: "True",
+            opt2: "False"
+        },
+        answer: "opt1"
+    },
+    {
+        question: "Testing",
+
+        options: {
+            opt1: "True",
+            opt2: "False"
+        },
+        answer: "opt1"
+    },
+    {
+        question: "The earth is flat",
+
+        options: {
+            opt1: "True",
+            opt2: "False"
+
+        },
+        answer: "False"
+    },
+    {
+        question: "What is da thingg?",
+
+        options: {
+            opt1: "True",
+            opt2: "False"
+        },
+        answer: "opt1"
+    },
+    {
+        question: "Testing",
+
+        options: {
+            opt1: "True",
+            opt2: "False"
+        },
+        answer: "opt1"
+    },
+    {
+        question: "The earth is flat",
+
+        options: {
+            opt1: "True",
+            opt2: "False"
+
+        },
+        answer: "False"
+    },
+    {
+        question: "What is da thingg?",
+
+        options: {
+            opt1: "True",
+            opt2: "False"
+        },
+        answer: "opt1"
+    },
+    {
+        question: "Testing",
+
+        options: {
+            opt1: "True",
+            opt2: "False"
+        },
+        answer: "opt1"
+    },
 ]
 
 let darkness = false;
-
 let modeSwitch = document.getElementById("darkmode");
-
+let quest = document.getElementById("questions");
 
 //----Darkmode button
 modeSwitch.addEventListener("click", () => {
@@ -41,13 +125,52 @@ modeSwitch.addEventListener("click", () => {
 
 
 
-console.log(Object.keys(theQuestions[1]))
 
-theQuestions.forEach((quiz) => {
-    console.log(quiz.question);
-    for (opt in quiz.options) {
-        console.log(opt);
-    }
-    console.log("Rätt svar är: " + quiz.answer)
 
-})
+
+
+
+function createForm(questions) {
+    questions.forEach((quiz) => {
+
+
+        let question = document.createElement("li");
+        question.innerHTML = quiz.question;
+        quest.append(question);
+        let ul = document.createElement("ul");
+
+
+
+        console.log(quiz.question);
+
+        for (opt in quiz.options) {
+            console.log(quiz.options[opt]);
+            let option = quiz.options[opt];
+
+
+            ul.innerHTML += ` 
+            
+            <li>
+            <label for="${option}"> ${option} </label>
+             <input type="checkbox" name="${option}"
+              id="${option}" value="${option}">
+            
+           </li>
+            
+            `;
+            question.append(ul);
+
+        }
+        console.log("Rätt svar är: " + quiz.answer)
+
+    })
+
+}
+
+function displayResult(){
+    
+}
+
+
+
+createForm(monoQuestions);
