6ee67a9285205713f74c5eae6648cda801dfd0e9
Albin Lundström <albin.wilhelm.erik@gmail.com>
2022-10-20 18:40:04 +0200

    Känner mig bra korkad men är så got som klar med G kriterierna, lite att förfina Messy atm , but made sure that all the radio buttons worked as intented, and now they do The function to add questions is complete the function to check answers, color them and show final score is added and complete will need cleaning up, but afaik, all criterias have been met will need to put in actuall questions and answers and not random drivel

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ let monoQuestions = [
             opt1: "True",
             opt2: "False"
         },
-        answer: "opt1"
+        answer: "True"
     },
     {
         question: "Testing",
@@ -25,7 +25,35 @@ let monoQuestions = [
             opt1: "True",
             opt2: "False"
         },
-        answer: "opt1"
+        answer: "True"
+    },
+    {
+        question: "The earth is flat",
+
+        options: {
+            opt1: "True",
+            opt2: "False"
+
+        },
+        answer: "True"
+    },
+    {
+        question: "What is da thingg?",
+
+        options: {
+            opt1: "True",
+            opt2: "False"
+        },
+        answer: "True"
+    },
+    {
+        question: "Testing",
+
+        options: {
+            opt1: "True",
+            opt2: "False"
+        },
+        answer: "False"
     },
     {
         question: "The earth is flat",
@@ -44,7 +72,7 @@ let monoQuestions = [
             opt1: "True",
             opt2: "False"
         },
-        answer: "opt1"
+        answer: "True"
     },
     {
         question: "Testing",
@@ -53,7 +81,7 @@ let monoQuestions = [
             opt1: "True",
             opt2: "False"
         },
-        answer: "opt1"
+        answer: "False"
     },
     {
         question: "The earth is flat",
@@ -63,7 +91,7 @@ let monoQuestions = [
             opt2: "False"
 
         },
-        answer: "False"
+        answer: "True"
     },
     {
         question: "What is da thingg?",
@@ -72,7 +100,7 @@ let monoQuestions = [
             opt1: "True",
             opt2: "False"
         },
-        answer: "opt1"
+        answer: "True"
     },
     {
         question: "Testing",
@@ -80,36 +108,8 @@ let monoQuestions = [
         options: {
             opt1: "True",
             opt2: "False"
-        },
-        answer: "opt1"
-    },
-    {
-        question: "The earth is flat",
-
-        options: {
-            opt1: "True",
-            opt2: "False"
-
         },
         answer: "False"
-    },
-    {
-        question: "What is da thingg?",
-
-        options: {
-            opt1: "True",
-            opt2: "False"
-        },
-        answer: "opt1"
-    },
-    {
-        question: "Testing",
-
-        options: {
-            opt1: "True",
-            opt2: "False"
-        },
-        answer: "opt1"
     },
 ]
 
@@ -122,55 +122,120 @@ modeSwitch.addEventListener("click", () => {
     document.body.classList.toggle("darkmode");
 })
 
+let showResults = document.getElementById("displayResults")
 
 
-
-
+let finalScore = document.getElementById("finalScore");
 
 
 
 
 function createForm(questions) {
+    let qCounter = 1;
     questions.forEach((quiz) => {
 
 
-        let question = document.createElement("li");
-        question.innerHTML = quiz.question;
+
+
+        let question = document.createElement("div");
+        question.id = "question" + qCounter;
+        question.innerHTML = ` <h4> Question ${qCounter} </h4 >
+            ${quiz.question}
+
+        `
+
+
+
+
         quest.append(question);
-        let ul = document.createElement("ul");
+
+        let choice = document.createElement("div");
+        question.append(choice)
 
 
 
-        console.log(quiz.question);
+
+
 
         for (opt in quiz.options) {
-            console.log(quiz.options[opt]);
+            //console.log(quiz.options[opt]);
             let option = quiz.options[opt];
 
+            choice.innerHTML += `
 
-            ul.innerHTML += ` 
-            
-            <li>
-            <label for="${option}"> ${option} </label>
-             <input type="checkbox" name="${option}"
-              id="${option}" value="${option}">
-            
-           </li>
-            
-            `;
-            question.append(ul);
+
+            <label label > ${option}
+        <input type="radio" name="Answer${qCounter}"
+            id="${option}${qCounter}" value="${option}" checked >
+        </label>
+
+
+        `;
+            // quest.append(choice);
 
         }
-        console.log("Rätt svar är: " + quiz.answer)
+        qCounter++;
 
     })
 
 }
 
-function displayResult(){
-    
-}
+
 
 
 
 createForm(monoQuestions);
+
+
+function displayResult(questions) {
+    let counter = 1;
+    let score = 0;
+    questions.forEach((quiz) => {
+
+
+        let userAnswer = document.querySelector("[name=Answer" + counter + "]:checked");
+
+        if (userAnswer.value === quiz.answer) {
+            score++
+            console.log("rätt")
+            document.getElementById("question" + counter).style.color = "green";
+        } else {
+            document.getElementById("question" + counter).style.color = "red";
+        }
+
+        console.log("ditt svar : " + userAnswer.value)
+        console.log("rätt svar är " + quiz.answer)
+
+        counter++;
+
+
+
+
+        // let answer1 = document.getElementsByName("Answer" + counter)
+        // //console.log(answer1)
+        // console.log("ditt svar : " + answer2.value)
+        // console.log("rätt svar är " + quiz.answer)
+
+    })
+    counter--
+    finalScore.innerHTML = `Your score is : ${score} / ${counter}`;
+
+
+    if (score < counter * 0.5) {
+        console.log("Underkänt")
+        console.log(score)
+        console.log(counter * 0.5)
+        finalScore.style.color = "red";
+    } else if (score <= counter * 0.75) {
+        console.log("Godkänt")
+        console.log(counter * 0.75 + "   wtf")
+        finalScore.style.color = "orange";
+    } else {
+        console.log("Mycket Väl Godkänt")
+        finalScore.style.color = "green";
+    }
+
+
+}
+
+showResults.addEventListener("click", () => displayResult(monoQuestions))
