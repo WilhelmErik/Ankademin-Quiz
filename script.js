4da43ef5ab7568bd049573984314829718155481
Albin Lundström <albin.wilhelm.erik@gmail.com>
2022-10-21 17:22:49 +0200

    Should now meet all G criterias added actual questions

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 let monoQuestions = [
     {
-        question: "The earth is flat",
+        question: "The construction of the Eiffel tower was completed on March 31, 1887",
 
         options: {
             opt1: "True",
-            opt2: "False"
-
+            opt2: "False",
         },
         answer: "False"
     },
     {
-        question: "What is da thingg?",
+        question: "Lightning is seen before it’s heard because light travels faster than sound.",
 
         options: {
             opt1: "True",
@@ -19,7 +18,7 @@ let monoQuestions = [
         answer: "True"
     },
     {
-        question: "Testing",
+        question: "Vatican City is a country",
 
         options: {
             opt1: "True",
@@ -28,26 +27,7 @@ let monoQuestions = [
         answer: "True"
     },
     {
-        question: "The earth is flat",
-
-        options: {
-            opt1: "True",
-            opt2: "False"
-
-        },
-        answer: "True"
-    },
-    {
-        question: "What is da thingg?",
-
-        options: {
-            opt1: "True",
-            opt2: "False"
-        },
-        answer: "True"
-    },
-    {
-        question: "Testing",
+        question: "Melbourne is the capital of Australia",
 
         options: {
             opt1: "True",
@@ -56,17 +36,34 @@ let monoQuestions = [
         answer: "False"
     },
     {
-        question: "The earth is flat",
+        question: "Mount Fuji is the highest mountain in Japan",
 
         options: {
             opt1: "True",
             opt2: "False"
+        },
+        answer: "True"
+    },
+    {
+        question: "Broccoli contains more vitamin C than lemons.",
 
+        options: {
+            opt1: "True",
+            opt2: "False"
+        },
+        answer: "True"
+    },
+    {
+        question: "The skull is the strongest bone in the human body",
+
+        options: {
+            opt1: "True",
+            opt2: "False"
         },
         answer: "False"
     },
     {
-        question: "What is da thingg?",
+        question: "Google was initially called BackRub",
 
         options: {
             opt1: "True",
@@ -75,7 +72,7 @@ let monoQuestions = [
         answer: "True"
     },
     {
-        question: "Testing",
+        question: "The black box in a plane is black",
 
         options: {
             opt1: "True",
@@ -84,17 +81,7 @@ let monoQuestions = [
         answer: "False"
     },
     {
-        question: "The earth is flat",
-
-        options: {
-            opt1: "True",
-            opt2: "False"
-
-        },
-        answer: "True"
-    },
-    {
-        question: "What is da thingg?",
+        question: "Tomatoes are fruit",
 
         options: {
             opt1: "True",
@@ -103,7 +90,16 @@ let monoQuestions = [
         answer: "True"
     },
     {
-        question: "Testing",
+        question: "Depression is the leading cause of disability worldwide",
+
+        options: {
+            opt1: "True",
+            opt2: "False"
+        },
+        answer: "True"
+    },
+    {
+        question: "In Ohio, USA, it is illegal to get a fish drunk",
 
         options: {
             opt1: "True",
@@ -123,56 +119,29 @@ modeSwitch.addEventListener("click", () => {
 })
 
 let showResults = document.getElementById("displayResults")
-
-
 let finalScore = document.getElementById("finalScore");
-
-
-
 
 function createForm(questions) {
     let qCounter = 1;
     questions.forEach((quiz) => {
 
-
-
-
         let question = document.createElement("div");
         question.id = "question" + qCounter;
         question.innerHTML = ` <h4> Question ${qCounter} </h4 >
-            ${quiz.question}
-
-        `
-
-
-
+            ${quiz.question}`
 
         quest.append(question);
 
         let choice = document.createElement("div");
         question.append(choice)
 
-
-
-
-
-
         for (opt in quiz.options) {
-            //console.log(quiz.options[opt]);
             let option = quiz.options[opt];
-
             choice.innerHTML += `
-
-
             <label label > ${option}
         <input type="radio" name="Answer${qCounter}"
             id="${option}${qCounter}" value="${option}" checked >
-        </label>
-
-
-        `;
-            // quest.append(choice);
-
+        </label>`;
         }
         qCounter++;
 
@@ -180,18 +149,12 @@ function createForm(questions) {
 
 }
 
-
-
-
-
 createForm(monoQuestions);
-
 
 function displayResult(questions) {
     let counter = 1;
     let score = 0;
     questions.forEach((quiz) => {
-
 
         let userAnswer = document.querySelector("[name=Answer" + counter + "]:checked");
 
@@ -207,34 +170,20 @@ function displayResult(questions) {
         console.log("rätt svar är " + quiz.answer)
 
         counter++;
-
-
-
-
-        // let answer1 = document.getElementsByName("Answer" + counter)
-        // //console.log(answer1)
-        // console.log("ditt svar : " + answer2.value)
-        // console.log("rätt svar är " + quiz.answer)
-
     })
     counter--
     finalScore.innerHTML = `Your score is : ${score} / ${counter}`;
 
-
     if (score < counter * 0.5) {
         console.log("Underkänt")
-        console.log(score)
-        console.log(counter * 0.5)
         finalScore.style.color = "red";
     } else if (score <= counter * 0.75) {
         console.log("Godkänt")
-        console.log(counter * 0.75 + "   wtf")
         finalScore.style.color = "orange";
     } else {
         console.log("Mycket Väl Godkänt")
         finalScore.style.color = "green";
     }
-
 
 }
 
