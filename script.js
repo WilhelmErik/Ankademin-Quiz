04bccb28454f7ebfccba7bc7e33fbef3187274e9
Albin Lundström <albin.wilhelm.erik@gmail.com>
2022-10-22 11:08:46 +0200

    Change of plans instead of storing different question types in separate arrays, they are now all stored in the same array input type was added to each object as to prevent calling functions multiple times

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ let monoQuestions = [
             opt1: "True",
             opt2: "False",
         },
-        answer: "False"
+        answer: "False",
+        inputType: "radio"
     },
     {
         question: "Lightning is seen before it’s heard because light travels faster than sound.",
@@ -15,7 +16,8 @@ let monoQuestions = [
             opt1: "True",
             opt2: "False"
         },
-        answer: "True"
+        answer: "True",
+        inputType: "radio"
     },
     {
         question: "Vatican City is a country",
@@ -24,7 +26,8 @@ let monoQuestions = [
             opt1: "True",
             opt2: "False"
         },
-        answer: "True"
+        answer: "True",
+        inputType: "radio"
     },
     {
         question: "Melbourne is the capital of Australia",
@@ -33,7 +36,8 @@ let monoQuestions = [
             opt1: "True",
             opt2: "False"
         },
-        answer: "False"
+        answer: "False",
+        inputType: "radio"
     },
     {
         question: "Mount Fuji is the highest mountain in Japan",
@@ -42,7 +46,8 @@ let monoQuestions = [
             opt1: "True",
             opt2: "False"
         },
-        answer: "True"
+        answer: "True",
+        inputType: "radio"
     },
     {
         question: "Broccoli contains more vitamin C than lemons.",
@@ -51,7 +56,8 @@ let monoQuestions = [
             opt1: "True",
             opt2: "False"
         },
-        answer: "True"
+        answer: "True",
+        inputType: "radio"
     },
     {
         question: "The skull is the strongest bone in the human body",
@@ -60,7 +66,8 @@ let monoQuestions = [
             opt1: "True",
             opt2: "False"
         },
-        answer: "False"
+        answer: "False",
+        inputType: "radio"
     },
     {
         question: "Google was initially called BackRub",
@@ -69,7 +76,8 @@ let monoQuestions = [
             opt1: "True",
             opt2: "False"
         },
-        answer: "True"
+        answer: "True",
+        inputType: "radio"
     },
     {
         question: "The black box in a plane is black",
@@ -78,7 +86,8 @@ let monoQuestions = [
             opt1: "True",
             opt2: "False"
         },
-        answer: "False"
+        answer: "False",
+        inputType: "radio"
     },
     {
         question: "Tomatoes are fruit",
@@ -87,7 +96,8 @@ let monoQuestions = [
             opt1: "True",
             opt2: "False"
         },
-        answer: "True"
+        answer: "True",
+        inputType: "radio"
     },
     {
         question: "Depression is the leading cause of disability worldwide",
@@ -96,7 +106,8 @@ let monoQuestions = [
             opt1: "True",
             opt2: "False"
         },
-        answer: "True"
+        answer: "True",
+        inputType: "radio"
     },
     {
         question: "In Ohio, USA, it is illegal to get a fish drunk",
@@ -105,9 +116,65 @@ let monoQuestions = [
             opt1: "True",
             opt2: "False"
         },
-        answer: "False"
+        answer: "False",
+        inputType: "radio"
+    },
+    {
+        question: "When was theconstruction of the Eiffel tower completed",
+
+        options: {
+            opt1: "March 31, 1886",
+            opt2: "March 31, 1887",
+            opt3: "March 31, 1888",
+            opt4: "March 31, 1889"
+        },
+        answer: "March 31, 1889",
+        inputType: "checkbox"
+    },
+    {
+        question: "Which of the following are cities",
+
+        options: {
+            opt1: "Stockholm",
+            opt2: "Örebro",
+            opt3: "Borås",
+            opt4: "Malmö"
+        },
+        answer: "March 31, 1889",
+        inputType: "checkbox"
     },
 ]
+
+let multipleChoice1 = [
+    {
+        question: "When was theconstruction of the Eiffel tower completed",
+
+        options: {
+            opt1: "March 31, 1886",
+            opt2: "March 31, 1887",
+            opt3: "March 31, 1888",
+            opt4: "March 31, 1889"
+        },
+        answer: "March 31, 1889",
+        inputType: "checkbox"
+    },
+
+];
+
+let multipleChoice2 = [
+    {
+        question: "Which of the following are cities",
+
+        options: {
+            opt1: "Stockholm",
+            opt2: "Örebro",
+            opt3: "Borås",
+            opt4: "Malmö"
+        },
+        answer: "March 31, 1889",
+        inputType: "checkbox"
+    }
+];
 
 let darkness = false;
 let modeSwitch = document.getElementById("darkmode");
@@ -121,44 +188,53 @@ modeSwitch.addEventListener("click", () => {
 let showResults = document.getElementById("displayResults")
 let finalScore = document.getElementById("finalScore");
 
+let questionCounter = 1;
 function createForm(questions) {
-    let qCounter = 1;
+
     questions.forEach((quiz) => {
 
         let question = document.createElement("div");
-        question.id = "question" + qCounter;
-        question.innerHTML = ` <h4> Question ${qCounter} </h4 >
-            ${quiz.question}`
+        //question.className = "question"
+        question.id = "question" + questionCounter;
+        question.innerHTML = ` <h4> Question ${questionCounter} </h4 >
+           <p> ${quiz.question} </p>`
 
         quest.append(question);
-
+        let inputType = quiz.inputType;
         let choice = document.createElement("div");
         question.append(choice)
 
         for (opt in quiz.options) {
             let option = quiz.options[opt];
             choice.innerHTML += `
-            <label label > ${option}
-        <input type="radio" name="Answer${qCounter}"
-            id="${option}${qCounter}" value="${option}" checked >
+            <label class="option"> ${option}
+        <input type="${inputType}" name="Answer${questionCounter}"
+            id="${option}${questionCounter}" value="${option}" checked >
         </label>`;
         }
-        qCounter++;
+        questionCounter++;
 
     })
 
 }
 
-createForm(monoQuestions);
+createForm(monoQuestions, "radio");
+
+//createForm(multipleChoice1, "checkbox");
+
+//createForm(multipleChoice2, "checkbox");
+
+
+
 
 function displayResult(questions) {
-    let counter = 1;
     let score = 0;
+    let counter = 1;
     questions.forEach((quiz) => {
 
         let userAnswer = document.querySelector("[name=Answer" + counter + "]:checked");
-
-        if (userAnswer.value === quiz.answer) {
+        console.log(userAnswer)
+        if (quiz.answer.includes(userAnswer.value)) {
             score++
             console.log("rätt")
             document.getElementById("question" + counter).style.color = "green";
@@ -167,7 +243,7 @@ function displayResult(questions) {
         }
 
         console.log("ditt svar : " + userAnswer.value)
-        console.log("rätt svar är " + quiz.answer)
+        console.log("rätt svar är " + quiz.answer + "count" + counter)
 
         counter++;
     })
@@ -187,4 +263,7 @@ function displayResult(questions) {
 
 }
 
-showResults.addEventListener("click", () => displayResult(monoQuestions))
+showResults.addEventListener("click", () => {
+    displayResult(monoQuestions)
+
+})
